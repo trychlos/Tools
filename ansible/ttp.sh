7d7bd864a7bace9790ff2c18cf8948ff79b44013
Pierre Wieser <pwieser@trychlos.org>
2021-05-25 22:58:26 +0200

    Add Ansible sample

---
+++ ansible/ttp.sh
@@ -0,0 +1,18 @@
+# REQ-UNI-022:
+#  Install The Tools Project.
+#
+# NOTE:
+#  This file is managed by Ansible.
+#  It may have been templated (resp. installed, resp. configured) via
+#  an Ansible command, role or playbook.
+#  You should not modify it in place unless you are really sure you
+#  really know what you are doing.
+#
+# pwi 2017- 6-28 creation
+#
+# $Id: etc_profile.d_ttp.sh 5376 2017-07-17 11:02:38Z  $
+
+# Address the installed (standard) version of The Tools Project
+/opt/trychlos.pwi/TTP/tools/libexec/bootstrap/sh_bootstrap
+
+# vim:ts=4:sw=4
