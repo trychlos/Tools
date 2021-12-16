0c701c0f8973ca0e81ae797109d8c4e4ff55c2ed
Pierre Wieser <pwieser@trychlos.org>
2021-12-16 08:44:02 +0100

    ttp.sh: consider initializing the environment from a crontab

--- tools/bin/ttp.sh
+++ tools/bin/ttp.sh
@@ -21,6 +21,7 @@
 # pwi 2012- 7-12 creation
 # pwi 2017- 6-21 publish the release at last
 # pwi 2021- 5-25 get rid of TTP_SHDIR variable
+# pwi 2021-12-16 consider running from a crontab where shell is fully addressed
 
 # In order to be able to honor the 'logical machine' paradigm (which
 # let the administrator have several execution nodes in the same machine),
@@ -48,6 +49,9 @@
 #   - we are in the user shell, which makes sure the command is actually
 #     ". ttp.sh ..."
 #   - we have to pre-load the function in a sub-environment.
+#
+# As a particular case, the ${0} argument is the full path of the current shell
+# when the environment is initialized from a crontab.
 
 #set -x
 #echo "ttp.sh: 0=$0 #=$# *=$*" >&2
@@ -56,8 +60,8 @@
 # Other in-process invocations may fall in error if FPATH is not honored
 # (ksh being the only known shell to be able to autoload its functions,
 #  typical sh-like shells, e.g. bash, would else fail) 
-if [ "${0:0:1}" = "-" -o "${0}" = "${SHELL##*/}" ]; then
-	[ "${1}" = "switch" ] && { . $(echo "${FPATH}" | awk -F: '{ print $1 }')/../bootstrap/sh_switch "$(which ttp.sh 2>/dev/null)" "${@}"; return $?; }
+if [ "${0:0:1}" == "-" -o "${0}" == "${SHELL}" -o "${0##*/}" == "${SHELL##*/}" ]; then
+	[ "${1}" == "switch" ] && { . $(echo "${FPATH}" | awk -F: '{ print $1 }')/../bootstrap/sh_switch "$(which ttp.sh 2>/dev/null)" "${@}"; return $?; }
 fi
 
 ttpf_main "${0}" "${@}"
